6a6bf2597ae28fb496d75d053febc099f27c8255
Gustavo <gustavoalvim85@gmail.com>
2023-04-20 19:26:55 -0300

    updating features section and styling

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -4,6 +4,7 @@ import './styles.sass';
 import Header from '../../components/Header';
 
 import hero from '../../assets/hero.svg'
+import dolar from '../../assets/dollar.svg'
 
 const Home = () => {
   return (
@@ -41,7 +42,52 @@ const Home = () => {
 
         <section id='Features' className='features'>
           <h2>Features of Kejoo app</h2>
-          <p>Kejoo provides many app features to make it easier for companies to manage company performance.</p>
+          <p className='description'>Kejoo provides many app features to make it easier for companies to manage company performance.</p>
+          <div className='cards'>
+            <div className='card'> 
+              <div className='box-icon'>
+                <img 
+                  src={dolar} 
+                  alt='' 
+                />
+              </div>
+              <h3>Calculate salary and other benefits</h3>
+              <p>Kejoo's employee payroll system is accessed online, making it easy to calculate salaries with various benefit components.</p>
+            </div>
+            <div className='card'> 
+              <div className='box-icon'>
+                <img 
+                  src={dolar} 
+                  alt='' 
+                />
+              </div>
+              <h3>Record employee attendance</h3>
+              <p>Optimized employee information and attendance recording and easy to calculate online attendance directly from the app.</p>
+            </div>
+            <div className='card'> 
+              <div className='box-icon'>
+                <img 
+                  src={dolar} 
+                  alt='' 
+                />
+              </div>
+              <h3>Calculate salary and other benefits</h3>
+              <p>Kejoo's employee payroll system is accessed online, making it easy to calculate salaries with various benefit components.</p>
+            </div>
+            <div className='card'> 
+              <div className='box-icon'>
+                <img 
+                  src={dolar} 
+                  alt='' 
+                />
+              </div>
+              <h3>Calculate salary and other benefits</h3>
+              <p>Kejoo's employee payroll system is accessed online, making it easy to calculate salaries with various benefit components.</p>
+            </div>
+          </div>
+        </section>
+        <section className='benefits'>
+          benefits
         </section>
       </main>
     </div>
