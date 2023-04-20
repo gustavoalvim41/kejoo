5d7b8502340ab6b2fa66036cdd028e4cb087c7b1
Gustavo <gustavoalvim85@gmail.com>
2023-04-20 01:17:01 -0300

    updating numbers class and styling

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -16,8 +16,19 @@ const Home = () => {
             <h1>Manage payroll & HR in your business so easily!</h1>
             <p>Kejoo's complete features make it easier for HRDs to manage and increase employee productivity in various locations. Save time and money.</p>
             <button>Get started</button>
-            <div>
-
+            <div className='numbers'>
+              <div>
+                <h3>10K+</h3>
+                <p>Downloaded</p>
+              </div>
+              <div>
+                <h3>4,3</h3>
+                <p>User ratings</p>
+              </div>
+              <div>
+                <h3>300+</h3>
+                <p>Company joins</p>
+              </div>
             </div>
           </article>
           <article className='hero-img'>
