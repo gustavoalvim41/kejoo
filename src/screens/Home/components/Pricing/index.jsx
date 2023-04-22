35ef21eff0c464e9db36e92ba1087051a430c831
Gustavo <gustavoalvim85@gmail.com>
2023-04-22 16:01:22 -0300

    updating hero component numbers media screen

---
+++ src/screens/Home/components/Pricing/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.sass';
+
+const Pricing = () => {
+  return (
+    <section>
+      under construction
+    </section>
+  )
+}
+
+export default Pricing;
