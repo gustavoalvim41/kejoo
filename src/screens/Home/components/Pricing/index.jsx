3543602f52c345c2ac04718c949b6b680528aaf0
Gustavo <gustavoalvim85@gmail.com>
2023-04-23 14:02:32 -0300

    updating pricing component and styling

--- src/screens/Home/components/Pricing/index.jsx
+++ src/screens/Home/components/Pricing/index.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import './styles.sass';
 
+const Switch = () => {
+  return (
+    <div className='switch-wrapper'>
+      <p>Monthly</p>
+      <label class="switch">
+        <input type="checkbox" />
+        <span class="slider round"></span>
+      </label>
+      <p>Annualy</p>
+    </div>
+  )
+} 
+
 const Pricing = () => {
   return (
     <section className='pricing'>
-      under construction 🚧 🚧 🚧
+      <div className='title-subtitle'>
+        <h2>Price we offer</h2>
+        <p>Get the best package offer based on your business needs, regardless of the number of employees.</p>
+      </div>
+      
+      <Switch />
+
+      
     </section>
   )
 }
