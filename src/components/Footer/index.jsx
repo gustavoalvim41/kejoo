db020af2f76c337369aee5ffb78a79b6d0ba75e7
Gustavo <gustavoalvim85@gmail.com>
2023-04-23 16:33:04 -0300

    styling footer initial

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,7 +4,9 @@ import './styles.sass';
 const Footer = () => {
   return (
     <footer>
-      
+      <div className='container'>
+        <p>© 2023 Kejoo. All right reserved</p>
+      </div>
     </footer>
   )
 }
